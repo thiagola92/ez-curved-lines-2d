acd2937fbda73a207d170488f97b1000d1af5cc6
René van der Ark <r.van.der.ark@gmail.com>
2025-03-30 15:27:40 +0200

    exports tesselate parameters.

--- addons/curved_lines_2d/drawable_path_2d.gd
+++ addons/curved_lines_2d/drawable_path_2d.gd
@@ -1,5 +1,5 @@
-# A custom node that extends Path2D so it can be drawn as a Line2D
-# Original adapted code: https://www.hedberggames.com/blog/rendering-curves-in-godot
+## A custom node that extends Path2D so it can be drawn as a Line2D
+## Original adapted code: https://www.hedberggames.com/blog/rendering-curves-in-godot
 @tool
 extends Path2D
 class_name  DrawablePath2D
@@ -17,6 +17,22 @@ signal line_changed()
 ## If you set this to true, be alert for potential performance issues
 @export var update_curve_at_runtime: bool = false
 
+## Controls the paramaters used to divide up the line  in segments.
+## These settings are prefilled with the default values.
+@export_group("Tesselation settings")
+## Controls how many subdivisions a curve segment may face before it is considered approximate enough. 
+## Each subdivision splits the segment in half, so the default 5 stages may mean up to 32 subdivisions 
+## per curve segment. Increase with care!
+@export_range(1, 10) var max_stages : int = 5:
+	set(_max_stages):
+		max_stages = _max_stages
+		line_changed.emit()
+## Controls how many degrees the midpoint of a segment may deviate from the real curve, before the 
+## segment has to be subdivided.
+@export_range(0.0, 180.0) var tolerance_degrees := 4.0:
+	set(_tolerance_degrees):
+		tolerance_degrees = _tolerance_degrees
+		line_changed.emit()
 
 # Wire up signals at runtime
 func _ready():
@@ -49,4 +65,4 @@ func _on_line_changed():
 # Redraw the line based on the new curve, using its tesselate method
 func curve_changed():
 	if is_instance_valid(line):
-		line.points = curve.tessellate()
+		line.points = curve.tessellate(max_stages, tolerance_degrees)
