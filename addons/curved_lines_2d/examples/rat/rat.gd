704a7e7aee1ba164154f10aca3e2b64bb11e2cad
René van der Ark <r.van.der.ark@gmail.com>
2025-05-04 08:39:36 +0200

    drops rat example script.

--- addons/curved_lines_2d/examples/rat/rat.gd
+++ addons/curved_lines_2d/examples/rat/rat.gd
@@ -4,19 +4,3 @@ extends CharacterBody2D
 
 func _ready() -> void:
 	$AnimationPlayer.play("run")
-	velocity.x = 800.0
-
-
-func _process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	move_and_slide()
-
-func disappear():
-	velocity.x = 0
-	$AnimationPlayer.play("disappear")
-
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "disappear":
-		queue_free()
