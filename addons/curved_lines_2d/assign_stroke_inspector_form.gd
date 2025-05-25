268d618c3f806fb52018e4eaab362086c0ffe433
René van der Ark <r.van.der.ark@gmail.com>
2025-05-25 16:26:51 +0200

    Finishes stroke inspector subform.

--- addons/curved_lines_2d/assign_stroke_inspector_form.gd
+++ addons/curved_lines_2d/assign_stroke_inspector_form.gd
@@ -4,24 +4,59 @@ extends Control
 var scalable_vector_shape_2d : ScalableVectorShape2D
 var create_button : Button
 var remove_button : Button
+var title_button : Button
+var collapse_icon : Texture2D
+var expand_icon : Texture2D
+var collapsible_siblings : Array[Node]
+var color_button : ColorPickerButton
+var stroke_width_input : EditorSpinSlider
 
 func _enter_tree() -> void:
 	if not is_instance_valid(scalable_vector_shape_2d):
 		return
-
+	collapse_icon = preload("res://addons/curved_lines_2d/Collapse.svg")
+	expand_icon = preload("res://addons/curved_lines_2d/Expand.svg")
 	create_button = find_child("CreateStrokeButton")
 	remove_button = find_child("RemoveStrokeButton")
+	title_button = find_child("TitleButton")
+	color_button = find_child("ColorPickerButton")
 	scalable_vector_shape_2d.assigned_node_changed.connect(_on_svs_assignment_changed)
+	collapsible_siblings = get_children().filter(func(x): return x != title_button)
+	stroke_width_input = _make_float_input("Stroke Width", 0.0, 1.0, 100.0, "px")
+	find_child("StrokeWidthFloatFieldContainer").add_child(stroke_width_input)
 	_on_svs_assignment_changed()
+	stroke_width_input.value_changed.connect(_on_stroke_width_changed)
 
 
 func _on_svs_assignment_changed() -> void:
 	if is_instance_valid(scalable_vector_shape_2d.line):
 		create_button.disabled = true
 		remove_button.disabled = false
+		stroke_width_input.value = scalable_vector_shape_2d.line.width
+		color_button.color = scalable_vector_shape_2d.line.default_color
 	else:
 		create_button.disabled = false
 		remove_button.disabled = true
+
+
+func _on_stroke_width_changed(new_value : float) -> void:
+	if not is_instance_valid(scalable_vector_shape_2d.line):
+		return
+	var undo_redo = EditorInterface.get_editor_undo_redo()
+	undo_redo.create_action("Adjust Line2D width for %s" % str(scalable_vector_shape_2d))
+	undo_redo.add_do_property(scalable_vector_shape_2d.line, 'width', new_value)
+	undo_redo.add_undo_property(scalable_vector_shape_2d.line, 'width', scalable_vector_shape_2d.line.width)
+	undo_redo.commit_action()
+
+
+func _on_color_picker_button_color_changed(color: Color) -> void:
+	if not is_instance_valid(scalable_vector_shape_2d.line):
+		return
+	var undo_redo = EditorInterface.get_editor_undo_redo()
+	undo_redo.create_action("Adjust Line2D width for %s" % str(scalable_vector_shape_2d))
+	undo_redo.add_do_property(scalable_vector_shape_2d.line, 'default_color', color)
+	undo_redo.add_undo_property(scalable_vector_shape_2d.line, 'default_color', scalable_vector_shape_2d.line.default_color)
+	undo_redo.commit_action()
 
 
 func _on_create_stroke_button_pressed():
@@ -33,6 +68,11 @@ func _on_create_stroke_button_pressed():
 	var line_2d := Line2D.new()
 	var root := EditorInterface.get_edited_scene_root()
 	var undo_redo = EditorInterface.get_editor_undo_redo()
+	line_2d.default_color = color_button.color
+	line_2d.width = stroke_width_input.value
+	line_2d.begin_cap_mode = Line2D.LINE_CAP_ROUND
+	line_2d.end_cap_mode = Line2D.LINE_CAP_ROUND
+	line_2d.joint_mode = Line2D.LINE_JOINT_ROUND
 	undo_redo.create_action("Add Line2D to %s " % str(scalable_vector_shape_2d))
 	undo_redo.add_do_method(scalable_vector_shape_2d, 'add_child', line_2d, true)
 	undo_redo.add_do_method(line_2d, 'set_owner', root)
@@ -41,3 +81,47 @@ func _on_create_stroke_button_pressed():
 	undo_redo.add_undo_method(scalable_vector_shape_2d, 'remove_child', line_2d)
 	undo_redo.add_undo_property(scalable_vector_shape_2d, 'line', null)
 	undo_redo.commit_action()
+
+
+func _on_remove_stroke_button_pressed() -> void:
+	if not is_instance_valid(scalable_vector_shape_2d):
+		return
+	if not is_instance_valid(scalable_vector_shape_2d.line):
+		return
+	var line_2d := scalable_vector_shape_2d.line
+	var undo_redo = EditorInterface.get_editor_undo_redo()
+	var root := EditorInterface.get_edited_scene_root()
+	var parent := line_2d.get_parent()
+	undo_redo.create_action("Remove Line2D from %s " % str(scalable_vector_shape_2d))
+	undo_redo.add_do_reference(line_2d)
+	undo_redo.add_do_method(parent, 'remove_child', line_2d)
+	undo_redo.add_do_property(scalable_vector_shape_2d, 'line', null)
+	undo_redo.add_undo_method(parent, 'add_child', line_2d)
+	undo_redo.add_undo_property(scalable_vector_shape_2d, 'line', line_2d)
+	undo_redo.add_undo_method(line_2d, 'set_owner', root)
+	undo_redo.commit_action()
+
+
+func _on_title_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		title_button.icon = collapse_icon
+		for n in collapsible_siblings:
+			n.show()
+	else:
+		title_button.icon = expand_icon
+		for n in collapsible_siblings:
+			n.hide()
+
+
+func _make_float_input(lbl : String, value : float, min_value : float, max_value : float, suffix : String) -> EditorSpinSlider:
+	var x_slider := EditorSpinSlider.new()
+	x_slider.value = value
+	x_slider.min_value = min_value
+	x_slider.max_value = max_value
+	x_slider.suffix = suffix
+	x_slider.label = lbl
+	x_slider.editing_integer = false
+	x_slider.step = 0.001
+	return x_slider
+
+
