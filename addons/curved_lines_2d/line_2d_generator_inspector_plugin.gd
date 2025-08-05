4da58cbd1d785d4a8572507cc1dc45816804b5b4
thiagola92 <thiagola92@gmail.com>
2025-08-05 15:17:54 -0300

    implement exporting as 'baked' scene

--- addons/curved_lines_2d/line_2d_generator_inspector_plugin.gd
+++ addons/curved_lines_2d/line_2d_generator_inspector_plugin.gd
@@ -32,6 +32,14 @@ func _parse_begin(object: Object) -> void:
 				assigned nodes outside this subtree will not be drawn."
 		add_custom_control(button)
 		button.pressed.connect(func(): _on_export_png_button_pressed(object))
+	if object is ScalableVectorShape2D:
+		var button : Button = Button.new()
+		button.text = "Export as baked scene*"
+		button.tooltip_text = "The export will only contain this node and its children,
+				assigned nodes outside this subtree will not be drawn.\n
+				Warning: AnimationPlayer will not be included."
+		add_custom_control(button)
+		button.pressed.connect(func(): _on_export_baked_scene_pressed(object))
 
 
 func _parse_group(object: Object, group: String) -> void:
@@ -109,6 +117,17 @@ func _on_export_png_button_pressed(svs : ScalableVectorShape2D) -> void:
 	dialog.popup_centered(Vector2i(800, 400))
 
 
+func _on_export_baked_scene_pressed(svs : ScalableVectorShape2D) -> void:
+	var dialog := EditorFileDialog.new()
+	dialog.add_filter("*.tscn", "Scene")
+	dialog.current_file = svs.name.to_snake_case()
+	dialog.file_mode = EditorFileDialog.FILE_MODE_SAVE_FILE
+	dialog.current_path = svs.name.to_lower()
+	dialog.file_selected.connect(func(path): _export_baked_scene(svs, path, dialog))
+	EditorInterface.get_base_control().add_child(dialog)
+	dialog.popup_centered(Vector2i(800, 400))
+
+
 func _export_png(svs : ScalableVectorShape2D, filename : String, dialog : Node) -> void:
 	dialog.queue_free()
 	var sub_viewport := SubViewport.new()
@@ -144,3 +163,109 @@ func _export_png(svs : ScalableVectorShape2D, filename : String, dialog : Node) 
 	img.save_png(filename)
 	EditorInterface.get_resource_filesystem().scan()
 	sub_viewport.queue_free()
+
+
+func _export_baked_scene(svs : ScalableVectorShape2D, filepath : String, dialog : Node) -> void:
+	dialog.queue_free()
+	
+	var svs_owner: Node = svs.owner
+	var svs_parent: Node = svs.get_parent()
+	var svs_index: int = svs.get_index()
+	var scene: PackedScene = _create_baked_scene(svs, filepath)
+	var node: Node2D = scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
+	
+	# We are replacing the entire branch.
+	svs_parent.remove_child(svs)
+	svs_parent.add_child(node, true)
+	svs_parent.move_child(node, svs_index)
+	
+	node.owner = svs_owner
+	node.scene_file_path = filepath
+	node.transform = svs.transform
+	
+	var undo_redo := EditorInterface.get_editor_undo_redo()
+	undo_redo.create_action("Export 'baked' scene")
+	undo_redo.add_do_method(svs_parent, "remove_child", svs)
+	undo_redo.add_undo_method(svs_parent, "remove_child", node)
+	undo_redo.add_do_method(svs_parent, "add_child", node, true)
+	undo_redo.add_undo_method(svs_parent, "add_child", svs, true)
+	undo_redo.add_do_method(svs_parent, "move_child", node, svs_index)
+	undo_redo.add_undo_method(svs_parent, "move_child", svs, svs_index)
+	undo_redo.add_do_property(node, "owner", svs_owner)
+	undo_redo.add_undo_property(svs, "owner", svs_owner)
+	undo_redo.add_do_method(self, "_replace_owner", node, null, svs_owner)
+	undo_redo.add_undo_method(self, "_replace_owner", svs, null, svs_owner)
+	undo_redo.add_do_reference(node)
+	undo_redo.add_undo_reference(svs)
+	undo_redo.commit_action(false)
+
+# It will temporarily modify the current branch so we can create the baked scene.
+func _create_baked_scene(svs : ScalableVectorShape2D, filepath: String) -> PackedScene:
+	var svs_owner: Node = svs.owner
+	var svs_children: Array[Node] = svs.get_children()
+	var svs_ownership: Array[Node]
+	var replace_map: Dictionary[Node, Node]
+	var root := Node2D.new()
+	root.name = svs.name
+	replace_map[svs] = root
+	
+	while svs_children.size() > 0:
+		var child: Node = svs_children.pop_back()
+		
+		if child is AnimationPlayer:
+			continue
+		
+		svs_children.append_array(child.get_children())
+		
+		if child.owner == svs_owner:
+			svs_ownership.append(child)
+		
+		if child is ScalableVectorShape2D:
+			var node := Node2D.new()
+			node.name = child.name
+			node.unique_name_in_owner = child.unique_name_in_owner
+			node.transform = child.transform
+			replace_map[child] = node
+	
+	_replace_nodes(replace_map)
+	
+	for child in svs_ownership:
+		if child in replace_map: # Got replaced, so update the replacement instead.
+			replace_map[child].owner = root
+		else:
+			child.owner = root
+	
+	var scene := PackedScene.new()
+	scene.pack(root)
+	ResourceSaver.save(scene, filepath, ResourceSaver.FLAG_NONE)
+	
+	# Undo modifications.
+	_replace_nodes(replace_map, true)
+	
+	for child in svs_ownership:
+		child.owner = svs_owner
+	
+	return ResourceLoader.load(filepath) as PackedScene
+
+# Replace nodes in the map by the counterpart (key <-> value).
+# NOTE: Important to know that doing "x.replace_by(y)"
+# will also update children owner to y if they were owned by x.
+func _replace_nodes(map: Dictionary[Node, Node], reverse: bool = false) -> void:
+	if reverse:
+		for node in map:
+			map[node].replace_by(node, true)
+	else:
+		for node in map:
+			node.replace_by(map[node], true)
+
+
+# NOTE: When you have a instance of a scene in the Scene dock, they do contain
+# all the same nodes from their original scene but they are hidden from the user.
+# This is done through Node.owner, because Scene dock only display
+# nodes owned by the root node.
+func _replace_owner(node: Node, old_owner: Node, new_owner: Node) -> void:
+	for child in node.get_children():
+		if child.owner == old_owner:
+			child.owner = new_owner
+		
+		_replace_owner(child, old_owner, new_owner)
