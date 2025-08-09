1f823a3d70232807b4daba2af8a5ad264d320a28
René van der Ark <r.van.der.ark@gmail.com>
2025-08-09 12:25:05 +0200

    makes drawing options available in click ellipse.

--- addons/curved_lines_2d/examples/add_shapes_programmatically/click_ellipse.gd
+++ addons/curved_lines_2d/examples/add_shapes_programmatically/click_ellipse.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@export var draw_collision := false
+@export var draw_strokes := true
+@export var draw_fills := true
+
+
 var foobar := false
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
@@ -38,17 +43,22 @@ func add_ellipse(at_pos: Vector2):
 	ellipse.shape_type = ScalableVectorShape2D.ShapeType.ELLIPSE
 	ellipse.position = at_pos
 	ellipse.size = Vector2(500, 250)
-	# assign a Polygon2D as fill
-	ellipse.polygon = Polygon2D.new()
-	ellipse.polygon.color = Color.WHITE
-	ellipse.add_child(ellipse.polygon)
-	# assign a Line2D as stroke
-	ellipse.line = Line2D.new()
-	ellipse.line.default_color = Color.BLACK
-	ellipse.add_child(ellipse.line)
 
+	if draw_fills:
+		# assign a Polygon2D as fill
+		ellipse.polygon = Polygon2D.new()
+		ellipse.polygon.color = Color.WHITE
+		ellipse.add_child(ellipse.polygon)
 
-	ellipse.collision_object = StaticBody2D.new()
-	ellipse.add_child(ellipse.collision_object)
+	if draw_strokes:
+		# assign a Line2D as stroke
+		ellipse.line = Line2D.new()
+		ellipse.line.default_color = Color.BLACK
+		ellipse.add_child(ellipse.line)
+
+	if draw_collision:
+		# assign a collision object to hold new collision polygons
+		ellipse.collision_object = StaticBody2D.new()
+		ellipse.add_child(ellipse.collision_object)
 
 	add_child(ellipse, true)
